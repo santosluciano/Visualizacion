4c6f531c7ddf426bd10dc71251354f6fb940f004
santosluciano <luchosan74@gmail.com>
2018-11-20 00:20:01 -0300

    se comenzo a maquetar sitio

---
+++ TP5/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('.carousel').carousel('pause');
+  $('.btncarousel').hover(function(event) {
+    event.preventDefault();
+    let id = $(this).attr("id");
+    $('.btncarousel').removeClass("mini-activo");
+    $(this).addClass("mini-activo");
+    $('.carousel-item').removeClass("active");
+    if (id == "img1")
+      $('#imagencarousel1').addClass("active");
+    else if ((id == "img2"))
+      $('#imagencarousel2').addClass("active");
+    else if ((id == "img3"))
+      $('#imagencarousel3').addClass("active");
+    else if ((id == "img4"))
+      $('#imagencarousel4').addClass("active");
+  });
+});

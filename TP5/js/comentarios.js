cd55c74caf0b2e7d988753f04f1f0529bfe0df6d
santosluciano <luchosan74@gmail.com>
2018-11-27 16:52:58 -0300

    animacion comentarios

--- TP5/js/comentarios.js
+++ TP5/js/comentarios.js
@@ -11,8 +11,9 @@ $(document).ready(function(){
     }
     
     function mostrarDatos(json) {
+      $(".contenedor-comentarios").html("");
       let html = '';
-      for (var i = 0; i < 3; i++) {
+      for (var i = 100; i < 103; i++) {
     
         html += '<div class="container">';
         html += '<div class="media comment-box">';
@@ -20,13 +21,21 @@ $(document).ready(function(){
         html += '<img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">'              
         html += '</div>';
         html += '<div class="media-body">';
-        html += '<h4 class="media-heading">'+json[i].email+'</h4>';
+        html += '<h4 class="media-heading">'+json[i].email+'<span class="fas fa-heart corazon"></span></h4>';
         html += '<p>'+json[i].body+'</p>';                               
         html += '</div></div></div>';    
       }
       let jqHTML = $(html);
       $(".contenedor-comentarios").html('').append(jqHTML);
-    
+      $('.corazon').on('click',function(){
+        $(this).addClass('animacion');
+      });     
+      $('.corazon.active').on('click',function(){
+        $(this).removeClass('animacion');
+        $(this).addClass('animacion-dislike');
+      });     
     }
+
+
 
 });

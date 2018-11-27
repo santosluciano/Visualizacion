00631ab563029839a602e9c5ec93d61660e23bbb
santosluciano <luchosan74@gmail.com>
2018-11-27 12:06:27 -0300

    se agregaron validaciones de login y direccionamiento de links

--- TP5/js/index.js
+++ TP5/js/index.js
@@ -22,7 +22,22 @@ $(document).ready(function () {
   //accion al hacer click en login
   $('.login').on('click', function(event){
     event.preventDefault();
-    window.location = "index-log.html";
+    let pass = $('#pass');
+    let mail = $('#mail');
+    let alerta = $('.alerta-login');
+    if (pass.val()=="admin" && mail.val()=="admin"){
+      pass.removeClass("error-input");
+      mail.removeClass("error-input");
+      alerta.removeClass("active");
+      $('.login-loading').html('<i class="fas fa-circle-notch fa-spin fa-2x"></i>');
+      setTimeout(function(){window.location = "index-log.html";},2000);0
+    }
+    else{
+      pass.addClass("error-input");
+      mail.addClass("error-input");
+      alerta.addClass("active");
+    }
+
   });
   $('.dropdown-juegos').on("mouseover",function(){
     $(this).dropdown('toggle');
@@ -47,5 +62,37 @@ $(document).ready(function () {
     $('.item-jugados.inactive').removeClass('inactive');
     $('.item-jugados.stand').addClass('inactive');
     $('.item-jugados.stand').removeClass('stand');
+  });
+  $('.page-item').on('click',function(event){
+    event.preventDefault();
+    let label = $(this).find('a').attr("aria-label");
+    if (label == 'Next'){
+      if ($('.page-item.active').next('li').find('a').attr("aria-label")!='Next'){
+        $('.page-item.active').next('li').addClass('prox-active');
+        $('.page-item').removeClass('active');
+        $('.prox-active').addClass('active');
+        $('.prox-active').removeClass('prox-active');
+      }
+    } else if (label =='Previous'){
+      if ($('.page-item.active').prev('li').find('a').attr("aria-label")!='Previous'){
+        $('.page-item.active').prev('li').addClass('prox-active');
+        $('.page-item').removeClass('active');
+        $('.prox-active').addClass('active');
+        $('.prox-active').removeClass('prox-active');
+      }
+    } 
+    else{
+      $('.page-item').removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+  $('.contenedor-juego img').on('click', function(){
+    window.location = 'juego.html';
+  });
+  $('.img-recomendado').on('click', function(){
+    window.location = 'juego.html';
+  });
+  $('.flechaabajo').on('click', function(){
+    window.location = 'todoslosjuegos.html';
   });
 });

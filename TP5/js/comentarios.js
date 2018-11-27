ee9d2daa8f8373badd798c9e168a4746b38de01b
santosluciano <luchosan74@gmail.com>
2018-11-27 20:28:45 -0300

    se agregaron link del nav del juego

--- TP5/js/comentarios.js
+++ TP5/js/comentarios.js
@@ -1,41 +1,41 @@
 $(document).ready(function(){
-    getDatos();
-
-
-    function getDatos() {
+    let i = 0;
+    getComentarios();
+    function getComentarios() {
         fetch('https://jsonplaceholder.typicode.com/comments')
         .then(response => response.json())
         .then(function (json) {
-          mostrarDatos(json);
+            mostrarComentarios(json);
         });  
     }
-    
-    function mostrarDatos(json) {
+    function mostrarComentarios(json) {
       $(".contenedor-comentarios").html("");
       let html = '';
-      for (var i = 100; i < 103; i++) {
-    
-        html += '<div class="container">';
-        html += '<div class="media comment-box">';
-        html += '<div class="media-left">';
-        html += '<img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">'              
-        html += '</div>';
-        html += '<div class="media-body">';
-        html += '<h4 class="media-heading">'+json[i].email+'<span class="fas fa-heart corazon"></span></h4>';
+      for (i; i <3; i++) {
+        html += '<div class="container"><div class="media comment-box"><div class="media-left">';
+        html += '<img class="img-responsive user-photo" src="images/usuario.png"></div><div class="media-body">'              
+        html += '<h4 class="media-heading">'+json[i].email+'<span class="fas fa-heart corazon animacion-dislike">'+calcularLikes()+'</span></h4>';
         html += '<p>'+json[i].body+'</p>';                               
         html += '</div></div></div>';    
       }
-      let jqHTML = $(html);
-      $(".contenedor-comentarios").html('').append(jqHTML);
-      $('.corazon').on('click',function(){
-        $(this).addClass('animacion');
-      });     
-      $('.corazon.active').on('click',function(){
-        $(this).removeClass('animacion');
-        $(this).addClass('animacion-dislike');
-      });     
+      let comHTML = $(html);
+      $(".contenedor-comentarios").html('').append(comHTML); 
     }
-
-
-
+    $(document).on('click','.animacion-dislike',function(){
+        $(this).removeClass('animacion-dislike');
+        $(this).addClass('animacion-corazon');
+        let mg = $(this).html();
+        mg = parseInt(mg);
+        $(this).html(mg+1);
+    });    
+    $(document).on('click','.animacion-corazon',function(){
+        $(this).removeClass('animacion-corazon');
+        $(this).addClass('animacion-dislike');
+        let mg = $(this).html();
+        mg = parseInt(mg);
+        $(this).html(mg-1);
+    });    
+    function calcularLikes(){
+        return Math.floor(Math.random() * (99 - 0)) + 0;
+    }
 });

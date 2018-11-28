97e4bef7014496e2efbde58f12b69ca4b2376520
santosluciano <luchosan74@gmail.com>
2018-11-28 00:03:00 -0300

    se cambio comportamiento dropdown juego

--- TP4/js/index.js
+++ TP4/js/index.js
@@ -1,5 +1,12 @@
 let juego = new Juego({id:"juego"});
 
 document.getElementById('jugar').addEventListener("click", function () {
-    juego.comenzar();
+    juego.comenzar();    
 });
+
+$('.dropdown-juegos').on("mouseover",function(){
+    $(this).dropdown('toggle');
+  });
+  $('.dropdown-juegos').on('click',function(){
+    window.location = 'https://santosluciano.github.io/Visualizacion/TP5/todoslosjuegos.html';
+  });

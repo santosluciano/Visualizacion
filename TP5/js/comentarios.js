dd04aff3598e9af791381ef393789f6fcdeb1681
santosluciano <luchosan74@gmail.com>
2018-11-27 23:10:27 -0300

    se simula comentarios en tiempo real

--- TP5/js/comentarios.js
+++ TP5/js/comentarios.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    let i = 30;
+    let i = 40;
     getComentarios();
     function getComentarios() {
         fetch('https://jsonplaceholder.typicode.com/comments')
@@ -11,8 +11,8 @@ $(document).ready(function(){
     function mostrarComentarios(json) {
       $(".contenedor-comentarios").html("");
       let html = '';
-      for (i; i <33; i++) {
-        html += '<div class="container"><div class="media comment-box"><div class="media-left">';
+      for (i; i <43; i++) {
+        html += '<div class="container contenedor-comentario"><div class="media comment-box"><div class="media-left">';
         html += '<img class="img-responsive user-photo" src="images/usuario.png"></div><div class="media-body">'              
         html += '<h4 class="media-heading">'+json[i].email+'<span class="fas fa-heart corazon animacion-dislike">'+calcularLikes()+'</span></h4>';
         html += '<p>'+json[i].body+'</p>';                               
@@ -20,6 +20,23 @@ $(document).ready(function(){
       }
       let comHTML = $(html);
       $(".contenedor-comentarios").html('').append(comHTML); 
+      let intervalo = setInterval(() => {
+        mostrarNuevoComentario(json);
+        if (i==400){
+            clearInterval(intervalo);
+        }
+       },3000);
+    }
+    function mostrarNuevoComentario(json){
+        $('.contenedor-comentario').last().remove();
+        let html = '';
+        html += '<div class="container contenedor-comentario"><div class="media comment-box"><div class="media-left">';
+        html += '<img class="img-responsive user-photo" src="images/usuario.png"></div><div class="media-body">'              
+        html += '<h4 class="media-heading">'+json[i].email+'<span class="fas fa-heart corazon animacion-dislike">'+calcularLikes()+'</span></h4>';
+        html += '<p>'+json[i].body+'</p>';                               
+        html += '</div></div></div>';   
+        i++;
+        $(".contenedor-comentarios").prepend(html); 
     }
     $(document).on('click','.animacion-dislike',function(){
         $(this).removeClass('animacion-dislike');
